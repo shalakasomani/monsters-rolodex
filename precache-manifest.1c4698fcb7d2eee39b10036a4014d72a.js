ddcdd7c8bf6e95fd1c0c6374d21fbf2681f98933
Shalaka Somani <shalaka_somani@intuit.com>
2019-09-14 16:45:34 +0530

    Updates

---
+++ precache-manifest.1c4698fcb7d2eee39b10036a4014d72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b142a826e80d8f5a21d1a5ad71821b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2848d24c63cec05c0059",
+    "url": "/monsters-rolodex/static/css/main.e7215ca8.chunk.css"
+  },
+  {
+    "revision": "32061f2f4fe1438d5314",
+    "url": "/monsters-rolodex/static/js/2.67be95f2.chunk.js"
+  },
+  {
+    "revision": "2848d24c63cec05c0059",
+    "url": "/monsters-rolodex/static/js/main.11a576ee.chunk.js"
+  },
+  {
+    "revision": "fd56c18d112aedca6c08",
+    "url": "/monsters-rolodex/static/js/runtime~main.837baca9.js"
+  }
+]);
